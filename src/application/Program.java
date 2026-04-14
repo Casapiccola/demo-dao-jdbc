c2c7ee77dadb5ee8f5be169aa132e868e124376c
Erik Casapiccola <contato.erikcasapiccola@gmail.com>
2026-04-13 21:21:39 -0300

    Update: add class Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
 
 public class Program {
     public static void main(String[] args) {
 
         Department department = new Department(1, "Books");
-
         System.out.println(department);
 
+        LocalDate date = LocalDate.now();
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", date, 3000.0, department);
+        System.out.println(seller);
     }
 }
