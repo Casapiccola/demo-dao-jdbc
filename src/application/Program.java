eb0ff3cb1f52a12c882f771b611a344cd3c2a792
Erik Casapiccola <contato.erikcasapiccola@gmail.com>
2026-04-13 22:37:40 -0300

    Seller insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,8 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 public class Program {
@@ -24,5 +26,10 @@ public class Program {
         System.out.println("\n===== TEST 3: seller findAll =====");
         List = sellerDao.findAll();
         List.forEach(System.out::println);
+
+        System.out.println("\n===== TEST 4: seller insert =====");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", LocalDate.now(), 4000.0,  department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " +  newSeller.getId());
     }
 }
