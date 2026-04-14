602eb271605692cf03b6816c966b4fae1e3bb041
Erik Casapiccola <contato.erikcasapiccola@gmail.com>
2026-04-13 21:52:20 -0300

    Update: findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,22 +2,15 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.time.LocalDate;
 
 public class Program {
     public static void main(String[] args) {
 
-        Department department = new Department(1, "Books");
-        System.out.println(department);
-
-        LocalDate date = LocalDate.now();
-
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", date, 3000.0, department);
-        System.out.println(seller);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
     }
 }
